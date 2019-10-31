d37f1e4481ecbe51a02dc5994ea03f5e90cf3bef
ismaeltech <edy1017@live.com>
2019-10-31 15:26:18 -0400

    Added success & failure message functions

--- assets/scripts/books/ui.js
+++ assets/scripts/books/ui.js
@@ -1,1 +1,20 @@
 'use strict'
+
+const successMessage = function (newText) {
+  $('#message').text(newText)
+  $('#message').removeClass('failure')
+  $('#message').addClass('success')
+  $('form').trigger('reset')
+}
+
+const failureMessage = function (newText) {
+  $('#message').text(newText)
+  $('#message').removeClass('success')
+  $('#message').addClass('failure')
+  $('form').trigger('reset')
+}
+
+module.exports = {
+  successMessage,
+  failureMessage
+}
