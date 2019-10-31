003ebb05f4762bd813f5eab2b8dfc2c6b60c8b63
asooge <asooge@gmail.com>
2019-10-31 15:32:18 -0400

    Got the books

--- assets/scripts/books/api.js
+++ assets/scripts/books/api.js
@@ -1,1 +1,13 @@
 'use strict'
+const config = require('../config')
+
+const allBooks = function () {
+  return $.ajax({
+    url: config.apiUrl + '/books',
+    method: 'GET'
+  })
+}
+
+module.exports = {
+  allBooks
+}
--- assets/scripts/books/events.js
+++ assets/scripts/books/events.js
@@ -1,1 +1,15 @@
 'use strict'
+const api = require('./api')
+const ui = require('./ui')
+
+const getBooks = function (event) {
+  console.log('get books button')
+  api.allBooks()
+    .then(ui.getBooksSuccess)
+    .catch(console.error)
+}
+
+
+module.exports = {
+  getBooks
+}
